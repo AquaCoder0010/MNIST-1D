3a2b9d60dca535b1a04ec6214a4aea8ffa98ae9c
Beemnet <aquacoder00100011@gmail.com>
2026-08-20 23:02:54 +0300

    added train.py

--- model.py
+++ model.py
@@ -4,15 +4,15 @@ import numpy as np
 # define a Linear unit class
 class Linear:
     def __init__(self, input_layer, output_layer):
-        self.weight = np.random.random((output_layer, input_layer))
-        self.bias = np.random.random((output_layer, 1))
+        self.weight = np.random.random((input_layer, output_layer))
+        self.bias = np.random.random((1, output_layer))
         pass;
 
     def forward(self, x):
         assert isinstance(x, np.ndarray) == True
         assert x.shape[1] == self.weight.shape[-1] and len(x.shape) != 1
         self.x_input = x
-        return x @ self.weight.T + self.bias.T    
+        return x @ self.weight + self.bias    
 
     def backward(self, grad):
         self.dW = grad.T @ self.x_input
---
+++ train.py
@@ -0,0 +1,53 @@
+from loss import *
+from model import *
+
+from mnist1d.data import make_dataset, get_dataset_args
+
+# a function in python that only takes in a list that contains objects init by those found in model.py
+# make sure to write an assert that satisfies this condition (an assert or anything else that is faster)
+def forward(x, model_layers):
+    assert all(isinstance(layer, (Linear, ReLU)) for layer in model_layers)
+    out = x
+    for layer in model_layers:
+        out = layer.forward(out)
+    return out
+
+# the data 
+defaults = get_dataset_args()
+data = make_dataset(defaults)
+x, y, t = data['x'], data['y'], data['t']
+
+mnist1d_model = [
+    Linear(40, 100), 
+    ReLU(), 
+    Linear(100, 100), 
+    ReLU(),
+    Linear(100, 10)
+]
+
+out = forward(x, mnist1d_model)
+criterion = MultiClassCrossEntropyLoss(10)
+loss = criterion.get_loss(mnist1d_model, x, y)
+
+epochs = 10
+batch_size = 16
+l_r = 0.1
+for i in range(epochs):
+    # random selection, this is a crude implementation (i.e if the batch size is not divisible it won't work, but it is night time and I am lazy to find another solution sooo)
+    indices = np.random.choice(x.shape[0], x.shape[0], replace=False).reshape(-1, batch_size)
+    for idx, inds in enumerate(indices):
+        x_b, y_b = x[inds], y[inds]
+
+        out = forward(x_b, mnist1d_model)
+        probs = criterion.softmax(out)
+        criterion.backward(mnist1d_model, x_b, y_b, l_r)
+        if idx % 10 == 0:
+            loss = criterion.get_loss(mnist1d_model, x_b, y_b)
+            print(f"Batch idx : {idx}, Loss: {loss}")
+
+
+    loss = criterion.get_loss(mnist1d_model, x, y)
+    print("="*10)
+    print(f"Finished training epoch : {i+1}")
+    print(f"Current Loss: {loss}")
+
